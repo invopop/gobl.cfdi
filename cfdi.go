88810b73b4f3b5f86c8b8ac5a03c79dc3c360ff3
Luismi Cavallé <luismi@lmcavalle.com>
2023-05-31 11:19:10 +0000

    Add minimal API

---
+++ cfdi.go
@@ -0,0 +1,11 @@
+package cfdi
+
+import (
+	"github.com/invopop/gobl"
+)
+
+type Document struct{}
+
+func (c *Converter) ConvertFromGOBL(env *gobl.Envelope) (*Document, error) {
+	return nil, nil
+}
---
+++ converter.go
@@ -0,0 +1,7 @@
+package cfdi
+
+type Converter struct{}
+
+func NewConverter() *Converter {
+	return new(Converter)
+}
